e1956df62e3df4feb5a703536a300706c6bfc2e0
antonferv <antonferv95@gmail.com>
2018-06-05 17:56:10 +0600

    df
    df

--- fpk Urist subbota/js/mainFPK.js
+++ fpk Urist subbota/js/mainFPK.js
@@ -57,10 +57,23 @@ $('.btn_orange ').click(function (e) {
       $(".JSChecknp").removeClass("np");
       $(".JSChecknpr").removeClass("npr");
 
+      $(".jsPositionBlockH3").each(function(){
+        $(this).parent(".servicesList_padR").parent(".servicesBlock").find(".servicesList_padL").prepend($(this));
+      });
+      $(".servicesList_padR").removeClass("greyLine");
+      $(".jsPositionBlockH3").addClass("h3js");
+
     }
     else{
+
       $(".JSChecknp").addClass("np");
       $(".JSChecknpr").addClass("npr");
+
+      $(".jsPositionBlockH3").each(function(){
+        $(this).parent(".servicesList_padL").parent(".servicesBlock").find(".servicesList_padR").prepend($(this));
+      });
+      $(".servicesList_padR").addClass("greyLine");
+      $(".jsPositionBlockH3").removeClass("h3js");
     }
   });
   $(window).resize();
@@ -135,32 +148,33 @@ $(".regular").slick({
 
 ]
 });
-var time = 2, cc=1;
-$(window).scroll(function(){
- $('#counter').each(function(){
-  var 
-  cPos=$(this).offset().top,
-  topWindow=$(window).scrollTop();
-  if(cPos<topWindow+800){
-   if(cc<2){
-    $('div').each(function(){
-     var 
-     i = 1,
-     num = $(this).data('num'),
-     step = 1000 * time / num,
-     that = $(this),
-     int = setInterval(function(){
-      if (i <= num) {
-       that.html(i);
-     }
-     else {
-       cc=cc+2;
-       clearInterval(int);
-     }
-     i++;
-   },step);
-   });};
-  };
-});
-});
+// var time = 2, cc=1;
+// $(window).scroll(function(){
+//  $('#counter').each(function(){
+//   var 
+//   cPos=$(this).offset().top,
+//   topWindow=$(window).scrollTop();
+//   if(cPos<topWindow+800){
+//    if(cc<2){
+//     $('div').each(function(){
+//      var 
+//      i = 1,
+//      num = $(this).data('num'),
+//      // 
+//      step = 10* time / num,
+//      that = $(this),
+//      int = setInterval(function(){
+//       if (i <= num) {
+//        that.html(i);
+//      }
+//      else {
+//        cc=cc+2;
+//        clearInterval(int);
+//      }
+//      i++;
+//    },step);
+//    });};
+//   };
+// });
+// });
 });
